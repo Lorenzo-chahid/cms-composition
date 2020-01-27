e80a526ae73dd554a0dec0740743e994b0bdd7bd
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2020-01-27 11:01:09 +0100

    init php blog list

---
+++ blog_list.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="./test.css">
+    </head>
+    <body>
+        <header>
+            <div class="pos-f-t">
+                <div class="svga">
+                    <img src="./img/logo-full.svg" />
+                </div>
+                
+                <nav class="navbar navbar-dark bg-dark">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                </nav>
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <div class="bg-dark p-4">
+                      <h4 class="text-white">Collapsed content</h4>
+                      <span class="text-muted">Toggleable via the navbar brand.</span>
+                    </div>
+                  </div>
+              </div>
+        </header>
+
+        <div class="container no-gutter">
+            <div class="row overflow">
+              <div class="col-md-6 col-sm-12 div2">
+                <img src="./img/01.jpg" class="img-fluid img-art">
+                <h3>Euismod Fermentum Nibh Lorem</h3>
+                <span>-03 décembre 2018</span>
+              </div>
+              <div class="col-md-6 col-sm-12 div1">
+                <img src="./img/02.jpg" class="img-fluid img-art">
+                <h3>Euismod Fermentum Nibh Lorem</h3>
+                <span>-03 décembre 2018</span>
+              
+              </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-6 col-sm-12 div2">
+                  <img src="./img/03.jpg" class="img-fluid img-art">
+                  <h3>Euismod Fermentum Nibh Lorem</h3>
+                <span>-03 décembre 2018</span>
+              
+                </div>
+                <div class="col-md-6 col-sm-12 div1">
+                  <img src="./img/04.jpg" class="img-fluid img-art">
+                  <h3>Euismod Fermentum Nibh Lorem</h3>
+                  <span>-03 décembre 2018</span>
+                </div>
+                
+              </div>
+              <div class="row mt-5">
+                <div class="col-md-6 col-sm-12 div2">
+                  <img src="./img/03.jpg" class="img-fluid img-art">
+                  <h3>Euismod Fermentum Nibh Lorem</h3>
+                <span>-03 décembre 2018</span>
+              
+                </div>
+                <div class="col-md-6 col-sm-12 div1">
+                  <img src="./img/04.jpg" class="img-fluid img-art">
+                  <h3>Euismod Fermentum Nibh Lorem</h3>
+                  <span>-03 décembre 2018</span>
+                </div>
+                
+              </div>
+
+              <div class=" row col-lg-12 content2">
+                <img src="./img/01.jpg" class=" testii img-fluid col-lg-4 col-sm-4" alt="Cinque Terre">
+                <div class="col-lg-4 col-sm-4 ">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus</div>
+                <div id="black" class="col-lg-4 col-sm-4">
+                    <div class="content-txt">
+                        <h4>Horaires</h4>
+                        <p>Lundi > dimanche :</p>
+                        <p>(Fermé le mardi)</p>
+                        <p>10h > 18h30</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
